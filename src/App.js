750e200fc0704dde1c468143e9c4872cce1df30d
geoffroy moreaud <g.moreaud@gmail.com>
2023-09-28 15:55:42 +0200

    creation de home + ajout d'axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pour l'importation de la page ne pas mettre de {}
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
-
+      {/* code a bien mettre entre les div */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
