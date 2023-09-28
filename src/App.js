bf1354b8a89b92ab14c3ab709c1950898dd9eaf9
geoffroy moreaud <g.moreaud@gmail.com>
2023-09-28 17:44:57 +0200

    finito

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// pour l'importation de la page ne pas mettre de {}
-import Home from "./pages/Home";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 const App = () => {
+  const [mealsData, setMealsData] = useState([]);
+  const [inputResult, setInputResult] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputResult)
+      .then((res) => setMealsData(res.data.meals));
+      // veux dire que le useEffect se rejouera lorsque la valeur de l'input changera
+  }, [inputResult]);
+
   return (
-    <div>
-      {/* code a bien mettre entre les div */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="app-container">
+      <h1>Welcome to the cooking App</h1>
+      <input
+        type="text"
+        placeholder="Entrez le nom de votre aliment"
+        onChange={(e) => setInputResult(e.target.value)}
+      />
+      <div className="meals-container">
+        {mealsData.map((meal) => (
+          <Card key={meal.idMeal} meal={meal} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Card = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+const Card = ({ meal }) => {
+  return(
+  <div className="meal-card">
+    <h2>{meal.strMeal}</h2>
+    <p>Origin: {meal.strArea}</p>
+    <img src={meal.strMealThumb} alt={"photo" + meal.strMeal} />
+    <p>{meal.strInstructions}</p>
+  </div>
+
+)};
 
 export default Card;
